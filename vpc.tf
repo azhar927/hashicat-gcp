fda3eab14f5f05abc13331ae1d00eb7fa48cfcaa
azhar927 <azhar4703@gmail.com>
2021-10-17 01:54:46 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "azhar927/network/google"
+  version = "3.4.0"
+  network_name = "gaurav-network"
+  project_id = "var.project"
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
